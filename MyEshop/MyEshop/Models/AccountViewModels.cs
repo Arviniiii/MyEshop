ccdf4e569bd5465c1c67cc65fbb810e49ff677ab
Arvin VahidKia <arvin321.vahidkia@gmail.com>
2020-08-31 13:40:00 +0430

    MyEshop

---
+++ MyEshop/MyEshop/Models/AccountViewModels.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEshop.Models
+{
+    public class RegisterViewModel
+    {
+       
+        [MaxLength(300)]
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Display(Name = "کلمه عبور")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Compare("Password" , ErrorMessage="کلمه عبور با تکرار کلمه عبور مطابقت ندارد")]
+        [Display(Name = "تکرار کلمه عبور")]
+        public string RePassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [MaxLength(300)]
+        [EmailAddress]
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Display(Name = "کلمه عبور")]
+        public string Password { get; set; }
+
+        [Display(Name = "مرا به خاطر بسپار ")]
+        public bool RememberMe { get; set; }
+    }
+}
